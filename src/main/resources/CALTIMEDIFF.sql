7bfd2a3be0f2298b2fc8a302dcd708b68b0089c9
Vikesh7547 <vikeshviki123@gmail.com>
2024-03-08 13:04:18 +0530

    formatted the procedure

--- src/main/resources/CALTIMEDIFF.sql
+++ src/main/resources/CALTIMEDIFF.sql
@@ -1,22 +1,21 @@
-CREATE OR REPLACE PROCEDURE CALTIMEDIFF(p_start_time         IN TIMESTAMP,
-                                    p_end_time           IN TIMESTAMP,
-                                   p_duration IN NUMBER,
-                                    p_flag               out varchar2,
-                                    p_out_msg            OUT VARCHAR2)
-                                   IS
-  v_available_start TIMESTAMP;
-  v_available_end   TIMESTAMP;
-  w_diff            INTERVAL DAY TO SECOND;
-  w_start_time      TIMESTAMP;
-  w_end_time        TIMESTAMP;
-  v_flag            BOOLEAN := TRUE;
-  W_COUNT           NUMBER;
-  w_start_time_new  TIMESTAMP;
-  w_end_time_new    TIMESTAMP;
-    w_available_startslot TIMESTAMP;
+CREATE OR REPLACE PROCEDURE CALTIMEDIFF(p_start_time IN TIMESTAMP,
+                                        p_end_time   IN TIMESTAMP,
+                                        p_duration   IN NUMBER,
+                                        p_flag       out varchar2,
+                                        p_out_msg    OUT VARCHAR2) IS
+  v_available_start     TIMESTAMP;
+  v_available_end       TIMESTAMP;
+  w_diff                INTERVAL DAY TO SECOND;
+  w_start_time          TIMESTAMP;
+  w_end_time            TIMESTAMP;
+  v_flag                BOOLEAN := TRUE;
+  W_COUNT               NUMBER;
+  w_start_time_new      TIMESTAMP;
+  w_end_time_new        TIMESTAMP;
+  w_available_startslot TIMESTAMP;
   w_available_endslot   TIMESTAMP;
-  wslot_time timestamp;
-  w_full_time timestamp;
+  wslot_time            timestamp;
+  w_full_time           timestamp;
 
 BEGIN
   p_flag := 'S';
@@ -27,15 +26,15 @@ BEGIN
   
     -- Attempt to find an existing appointment within the specified time range
   
-    SELECT min(a.startdate),max(a.enddate)
-    INTO v_available_start,v_available_end
-    FROM appointments a
+    SELECT min(a.startdate), max(a.enddate)
+      INTO v_available_start, v_available_end
+      FROM appointments a
     
      WHERE ((a.startdate BETWEEN w_start_time AND w_end_time) or
            (a.enddate BETWEEN w_start_time AND w_end_time)
            
            );
-
+  
     SELECT count(*)
       into W_COUNT
       FROM appointments a
@@ -44,73 +43,70 @@ BEGIN
            (a.enddate BETWEEN w_start_time AND w_end_time)
            
            );
-  if(W_COUNT>1) then
-   DBMS_OUTPUT.put_line('inside count');
-  else
+    if (W_COUNT > 1) then
+      DBMS_OUTPUT.put_line('inside count');
+    else
     
-   
-    SELECT a.startdate, a.enddate
-      INTO v_available_start, v_available_end
-      FROM appointments a
+      SELECT a.startdate, a.enddate
+        INTO v_available_start, v_available_end
+        FROM appointments a
+      
+       WHERE ((a.startdate BETWEEN w_start_time AND w_end_time) or
+             (a.enddate BETWEEN w_start_time AND w_end_time)
+             
+             )
+         and rownum = 1;
     
-     WHERE ((a.startdate BETWEEN w_start_time AND w_end_time) or
-           (a.enddate BETWEEN w_start_time AND w_end_time)
-           
-           )
-       and rownum = 1;
-     
-  end if;
+    end if;
   EXCEPTION
     WHEN NO_DATA_FOUND THEN
       v_flag := false;
-     p_flag    := 'S';
+      p_flag := 'S';
       -- Handle the case when no appointment is found
-    wslot_time:=w_start_time+  INTERVAL '1' MINUTE * p_duration;
-      p_out_msg := '  slot are avaible for given period .'||w_start_time||'to'||wslot_time;
-    
+      wslot_time := w_start_time + INTERVAL '1' MINUTE * p_duration;
+      p_out_msg  := '  slot are avaible for given period .' || w_start_time || 'to' ||
+                    wslot_time;
     
   END;
-  
 
   -- Check if the end time needs adjustment (it is greater than the available end time)
-  
+
   if w_start_time < v_available_start THEN
     -- Check if the start time needs adjustment (it is less than the available start time)
-    w_diff               := v_available_start - w_start_time;
-    w_start_time_new     := w_start_time + w_diff;
-      wslot_time:=w_start_time+  INTERVAL '1' MINUTE * p_duration;
-      if(wslot_time>w_start_time_new) then
-      p_out_msg            := ' The slot is fully booked.';
-                           
-       else
-      
-    p_out_msg            := 'Available slot: ' || w_start_time || 'TO' ||' '||
-                            wslot_time ||' '|| 'Do you wish to Cotinue';
-                            end if;
+    w_diff           := v_available_start - w_start_time;
+    w_start_time_new := w_start_time + w_diff;
+    wslot_time       := w_start_time + INTERVAL '1' MINUTE * p_duration;
+    if (wslot_time > w_start_time_new) then
+      p_out_msg := ' The slot is fully booked.';
+    
+    else
+    
+      p_out_msg := 'Available slot: ' || w_start_time || 'TO' || ' ' ||
+                   wslot_time || ' ' || 'Do you wish to Cotinue';
+    end if;
   
-     p_flag    := 'F';
+    p_flag := 'F';
     -- p_date:=w_start_time||'|'||w_start_time;
-    elsIF w_end_time >v_available_end THEN
-    w_diff               := w_end_time - v_available_end;
-    w_end_time_new       := w_end_time - w_diff;
-    wslot_time:=w_end_time_new+  INTERVAL '1' MINUTE * p_duration;
-     if(wslot_time>w_end_time) then
- 
-  p_out_msg            := ' The slot is fully booked.';
-  else
-
-    p_out_msg            := 'Available slot: ' || w_end_time_new ||' '|| 'to' ||' '||
-                            wslot_time ||' '|| 'Do you wish to Cotinue';
-  
-     p_flag    := 'F';
-     end if;
+  elsIF w_end_time > v_available_end THEN
+    w_diff         := w_end_time - v_available_end;
+    w_end_time_new := w_end_time - w_diff;
+    wslot_time     := w_end_time_new + INTERVAL '1' MINUTE * p_duration;
+    if (wslot_time > w_end_time) then
+    
+      p_out_msg := ' The slot is fully booked.';
+    else
+    
+      p_out_msg := 'Available slot: ' || w_end_time_new || ' ' || 'to' || ' ' ||
+                   wslot_time || ' ' || 'Do you wish to Cotinue';
+    
+      p_flag := 'F';
+    end if;
   
   ELSIF v_flag THEN
     p_flag    := 'F';
     p_out_msg := 'The slot is fully booked.';
   
   END IF;
- 
 
   -- Display the result
 END;
